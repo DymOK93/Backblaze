6b72e5c79a8bf407673252c2c69c2f3570295a90
Dmitry Bolshakov <d.bolshakov@bi.zone>
2024-02-01 05:29:11 +0300

    Don't use '-' in column names

--- backblaze.cpp
+++ backblaze.cpp
@@ -242,7 +242,7 @@ static vector<string> MakeParsedStatsHeader(const DataCenterStats& dc_stats) {
 
   for (auto year = kFirstYear; year <= kLastYear; ++year) {
     for (uint8_t month = 1; month <= kMonthPerYear; ++month) {
-      header.push_back(fmt::format("date_{}-{}", year, month));
+      header.push_back(fmt::format("date_{}_{}", year, month));
     }
   }
 
